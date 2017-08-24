f37bb1497d2b530d5e6366b3c7fd5b3246147fa0
Lukas Hoffmann <lahoffm@emory.edu>
2017-08-24 17:07:35 -0400

    test

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 print('Hello world')
 print("edit 1")
 print("edit 2")
 print("edit 3")
-=======
-print('Hello there')
-<<<<<<< HEAD
->>>>>>> parent of 267259a... recommitting the edit
-=======
->>>>>>> parent of 267259a... recommitting the edit
